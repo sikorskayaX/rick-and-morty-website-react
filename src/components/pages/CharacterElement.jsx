4a2ab39600f57c3136843564785e84e349ef3f24
sikorskayaX <sikorskaya02@gmail.com>
2024-07-27 03:18:49 +0300

    Add new components and Redux reducers
    More detailed explanation:
    
    This commit adds the following new components and Redux reducers to the project:
    
    1. `CharacterElement` component: Displays detailed information about a specific character.
    2. `Characters` component: Displays a list of characters with filtering and pagination functionality.
    3. `EpisodeElement` component: Displays detailed information about a specific episode.
    4. `Episodes` component: Displays a list of episodes with filtering and pagination functionality.
    5. `LocationElement` component: Displays detailed information about a specific location.
    6. `Locations` component: Displays a list of locations with filtering and pagination functionality.
    
    The commit also adds the following Redux reducers:
    
    1. `charactersReducer`: Manages the state for the character-related data and actions.
    2. `locationsReducer`: Manages the state for the location-related data and actions.
    3. `episodesReducer`: Manages the state for the episode-related data and actions.
    
    These components and reducers work together to provide a comprehensive user interface and state management for the Rick and Morty application.

---
+++ src/components/pages/CharacterElement.jsx
@@ -0,0 +1,98 @@
+﻿import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchEpisodes } from '../redux/episodesReducer';
+import { fetchLocations } from '../redux/locationsReducer';
+import { Link } from "react-router-dom";
+import GoBack from '../GoBack';
+
+const CharacterElement = () => {
+  const { characterId } = useParams();
+  const dispatch = useDispatch();
+  const character = useSelector((state) =>
+    state.characters.characters.find((c) => c.id.toString() === characterId)
+  );
+
+  const episodes = useSelector((state) => state.episodes.episodes);
+  const locations = useSelector((state) => state.locations.locations);
+
+  
+  useEffect(() => {
+    if (character && character.episode) {
+      dispatch(fetchEpisodes(character.episode));
+    }
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (character) {
+      dispatch(fetchLocations([character.location.url, character.origin.url]));
+    }
+  }, [dispatch, character]);
+
+
+  if (!character) {
+    return <div>Character not found</div>;
+  }
+  
+
+  return (
+    <body>
+      <main>
+        <GoBack/>
+        <div className='character__head'>
+          <img className='character__image' src={character.image} alt={character.name} />
+          <h1>{character.name}</h1>
+        </div>
+        <div className='character__properties'>
+          <div className='character__informations'>
+            <h2 className='character__informations-title'>Informations</h2>
+            <div className='character__information'>
+              <p className='character__info-title'>Gender</p>
+              <p className='small'>{character.gender}</p>
+            </div>
+            <div className='character__information'>
+              <p className='character__info-title'>Status</p>
+              <p className='small'>{character.status}</p>
+            </div>
+            <div className='character__information'>
+              <p className='character__info-title'>Species</p>
+              <p className='small'>{character?.species || 'unknown'}</p>
+            </div>
+            <div className='character__information'>
+              <p className='character__info-title'>Type</p>
+              <p className='small'>{character?.type || 'unknown'}</p>
+            </div>
+            <Link to={locations[0] ? `/locations/${locations[0].id}` : '#!'}>
+              <div className='character__information-clickable'>
+                <p className='character__info-title'>Planet</p>
+                <p className='small'>{locations[0]?.name || 'unknown'}</p>
+              </div>
+            </Link>
+            <Link to={locations[1] ? `/locations/${locations[1].id}` : '#!'}>
+              <div className='character__information-clickable'>
+                <p className='character__info-title'>Origin</p>
+                <p className='small'>{locations[1]?.name || 'unknown'}</p>
+              </div>
+            </Link>
+          </div>
+          <div className='character__episodes'>
+            <h2 className='character__informations-title'>Episodes</h2>
+            <div className="character__episodes-container" >
+            {episodes.map((episode) => (
+                <Link to={`/episodes/${episode.id}`}>
+                <div className='character__episode'key={episode.id}>
+                  <h2 className='character__info-title'>{episode.episode} </h2>
+                  <p className='small'>{episode.name}</p>
+                  <p className='little'>{episode.air_date}</p>
+                </div>
+                </Link>
+            ))}
+            </div>
+          </div>
+        </div>
+      </main>
+    </body>
+  );
+};
+
+export default CharacterElement;
